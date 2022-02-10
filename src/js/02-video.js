4d45123079f3480e67838a96240256c1447800c9
Ihor <ihorfront88@gmail.com>
2022-02-10 20:04:20 +0200

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const onPlay = ({ seconds }) => {
+    localStorage.setItem('videoplayer - current - time', seconds);
+};
+
+const onCurrentTime = () => {
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+    const currentSeconds = localStorage.getItem('videoplayer - current - time');
+
+    if (currentSeconds) {
+        player.setCurrentTime(currentSeconds);
+    }
+};
+onCurrentTime();
