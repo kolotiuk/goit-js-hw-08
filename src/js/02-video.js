9cde690fe4cd07cef4643327cf630f41be01d151
Ihor <ihorfront88@gmail.com>
2022-08-09 21:36:41 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = data => {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(currentTime);
